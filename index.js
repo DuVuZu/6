b58fd019c19e914569a0f66730077bc3d0c3c102
DuVuZu <163808772+DuVuZu@users.noreply.github.com>
2024-04-15 08:09:33 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,35 @@
+console.log('JS ЗАПУЩЕН')
+
+let btn = document - querySelector('.theme')
+let title = document - querySelector('.title')
+let txt = document -  querySelecto('.txt')
+let img = document -  querySelectoAII('.image')
+let tp = document -  querySelectoAII('.p')
+
+console.log("переменная theme",btn)
+console.log("переменная p",tp)
+console.log("переменная title",title)
+console.log("переменная txt",txt)
+console.log("переменная img",image)
+
+// function theme_change() {
+//     if(title.style.color == 'white'){
+//         document.body.style.backgroundColor = 'rgb(32,32,161);'
+//         title.stile.color = 'black'
+//         txt.stile.color = 'black'
+//         tp.stile.color = 'black'
+//         img.src = 'https://ir.ozone.ru/s3/multimedia-g/c1000/6657381124.jpg'
+//         btn.innerText = "Тема: Светлая"
+//     }else {
+//         document.body.style.backgroundColor = 'rgb(148,187,233);;'
+//         title.stile.color = 'white'
+//         txt.stile.color = 'white'
+//         tp.stile.color = 'white'
+//         btn.innerText = "Тема: Темная"
+//     }
+// }
+   
+
+
+
+// btn.addEventListener("click",theme_change)
